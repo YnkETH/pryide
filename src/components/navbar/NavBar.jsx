ed2bab892c687452ee3ea6facd60b332376f52d4
Giancarlo Moreno <yankomoreno1@gmail.com>
2023-07-23 23:41:03 -0500

    123

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react'
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react'
+
+import {  useDispatch } from 'react-redux';
 
 import style from "./NavBar.module.css"
 import { scrollNavBarArchivos, scrollNavBarPublicaciones,scrollNavBarConocenos } from '../../actions'

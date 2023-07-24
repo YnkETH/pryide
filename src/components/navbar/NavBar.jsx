ba5f6c99dccaab0618095b2a6cfb9370569d6d97
Giancarlo Moreno <yankomoreno1@gmail.com>
2023-07-24 00:09:04 -0500

    123

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
   return (
     <div id='navContainer' className={style.container}>
       <div className={style.divContainer} onClick={btnPublicaciones} >
-      <h1>Publicacionesss</h1> 
+      <h1>Publicaciones</h1> 
       </div>
       <div className={style.divContainer} onClick={btnArchivos}>
       <h1>Archivos</h1>  {/*(IMAGENES)*/} 
@@ -33,7 +33,8 @@ const NavBar = () => {
       <h1>Conócenos</h1> {/*(INFORMACIÓN DE NOSOTROS)*/} 
       </div>
       <div className={style.divContainerA} onClick={btnAdex}>
-      <h1>ADEX</h1>
+      {/* <h1>ADEX</h1> */}
+      <img src='https://scontent.flim12-1.fna.fbcdn.net/v/t1.18169-9/13903407_1033985716637694_9089162073519752579_n.png?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGQEdG0kgdE0bxvMbDnuxbpwdE2OeyuCLLB0TY57K4IsuDmFm0FtEJ0J5DNEB_nsuft9Ty2hITzAk35uf-R7f4D&_nc_ohc=QywPVMzfVS4AX_nhtp_&_nc_ht=scontent.flim12-1.fna&oh=00_AfAOBjjrEJQBFvZ41_tA5VJ_Haf9tSNwNM1SK-mpgHD1Kg&oe=64E57EC3'></img>
       </div>
   </div>
   )
